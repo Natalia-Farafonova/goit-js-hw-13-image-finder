e7057aa241f662bfa7758f1b99f099ce32e58586
Natalia-Farafonova <81919602+Natalia-Farafonova@users.noreply.github.com>
2021-10-23 13:49:02 +0000

    Deploying to gh-pages from @ Natalia-Farafonova/goit-js-hw-13-image-finder@091c14cf0e22ab817fcac5e88771105aa237b75b 🚀

--- src.cc6049e5.js
+++ src.cc6049e5.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/apiService");
 },{"./sass/main.scss":"clu1","./js/apiService":"eQwa"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.13eae737.js.map
+//# sourceMappingURL=/goit-js-hw-13-image-finder/src.cc6049e5.js.map
